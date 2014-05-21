93e8e0e0b18ebf05edc9bf0c4c64c9d37631a2f9
Jayanudin <jayanudin@mail.unpas.ac.id>
2014-05-21 15:50:31 +0700

    Update class Mahasiswa Testcase

---
+++ src/main/java/org/rkppl/Manusia.java
@@ -0,0 +1,33 @@
+package org.rkppl;
+
+public class Manusia {
+
+	private String nama;
+	private String umur;
+	private String jabatan;
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getUmur() {
+		return umur;
+	}
+
+	public void setUmur(String umur) {
+		this.umur = umur;
+	}
+
+	public String getJabatan() {
+		return jabatan;
+	}
+
+	public void setJabatan(String jabatan) {
+		this.jabatan = jabatan;
+	}
+
+}
