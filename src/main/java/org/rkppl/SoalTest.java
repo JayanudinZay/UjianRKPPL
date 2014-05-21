86629fb79133bae3c2ddbe3c5f667dfd9ce3171f
Jayanudin <jayanudin@mail.unpas.ac.id>
2014-05-21 16:23:14 +0700

    update

--- src/main/java/org/rkppl/SoalTest.java
+++ src/main/java/org/rkppl/SoalTest.java
@@ -2,12 +2,17 @@ package org.rkppl;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SoalTest {
 	
 	private Soal soal;
+	
+	public SoalTest(){
+		soal = new Soal();
+	}
 	
 	@Before
 	public  void awalTest(){
@@ -17,23 +22,24 @@ public class SoalTest {
 	
 	@Test
 	public void test1(){
-		System.out.println("8 pangkat 2 : "+soal.pangkat(8, 2));
-		assertSame("Seharusnya 64 : ", soal.pangkat(2, 2));
+		System.out.println("8 pangkat 2 : "+soal.pangkat(2, 2));
+		assertSame("Seharusnya 4 : ",4, soal.pangkat(2, 2));
 	}
 	
 	@Test
 	public void test2(){
 		System.out.println("2 dikali 2 : "+soal.perkalian(2, 2));
-		assertSame("Seharusnya 4 : ", soal.perkalian(2, 2));
+		assertSame("Seharusnya 4 : ",4, soal.perkalian(2, 2));
 	}
 	
 	@Test
 	public void test3(){
 		System.out.println("faktorial 2 : "+soal.faktorial(2));
-		assertSame("Seharusnya 4", soal.faktorial(2));
+		assertSame("Seharusnya 2",2, soal.faktorial(2));
 		
 	}
 	
+	@After
 	public void endTest(){
 		soal = new Soal();
 		System.out.println("Akhir Testing");
