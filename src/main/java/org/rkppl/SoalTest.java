490011bdf709bdd7937b48415b1b10b03e3971ec
Jayanudin <jayanudin@mail.unpas.ac.id>
2014-05-21 16:31:04 +0700

    update test case

--- src/main/java/org/rkppl/SoalTest.java
+++ src/main/java/org/rkppl/SoalTest.java
@@ -7,40 +7,51 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SoalTest {
-	
+
 	private Soal soal;
-	
-	public SoalTest(){
+
+	public SoalTest() {
 		soal = new Soal();
 	}
-	
+
 	@Before
-	public  void awalTest(){
+	public void awalTest() {
 		soal = new Soal();
 		System.out.println("Awal testing");
 	}
-	
+
 	@Test
-	public void test1(){
-		System.out.println("8 pangkat 2 : "+soal.pangkat(2, 2));
-		assertSame("Seharusnya 4 : ",4, soal.pangkat(2, 2));
+	public void test1() {
+		assertSame("Seharusnya 4 : ", 4, soal.pangkat(2, 2));
+	}
+
+	@Test
+	public void test2() {
+		assertSame("Seharusnya 4 : ", 4, soal.perkalian(2, 2));
+	}
+
+	@Test
+	public void test3() {
+		assertSame("Seharusnya 2", 2, soal.faktorial(2));
+
 	}
 	
 	@Test
-	public void test2(){
-		System.out.println("2 dikali 2 : "+soal.perkalian(2, 2));
-		assertSame("Seharusnya 4 : ",4, soal.perkalian(2, 2));
+	public void test4() {
+		assertNotSame("Seharusnya 6", 6, soal.pangkat(2, 3));
 	}
 	
 	@Test
-	public void test3(){
-		System.out.println("faktorial 2 : "+soal.faktorial(2));
-		assertSame("Seharusnya 2",2, soal.faktorial(2));
-		
+	public void test5() {
+		assertNotSame("seharusnya 6", 6, soal.perkalian(3, 3));
 	}
-	
+	@Test
+	public void test6() {
+		assertNotSame("seharusnya 24", 6, soal.faktorial(4));
+	}
+
 	@After
-	public void endTest(){
+	public void endTest() {
 		soal = new Soal();
 		System.out.println("Akhir Testing");
 	}
